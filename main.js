d8fd453410b1d52349ae0af4d359b65891793f8b
rrdmp15 <rdayismar@gmail.com>
2023-06-15 22:05:22 -0500

    feat: :rocket: Basic Project's Mockup

---
+++ main.js
@@ -0,0 +1,4 @@
+import myHeader from "./components/myHeader.js";
+import myFooter from "./components/myFooter.js";
+import myBanner from "./components/myBanner.js";
+import myArticles from "./components/myArticles.js";
