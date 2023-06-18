344ad73343962636368a3bf4e64e75912cf1b831
rrdmp15 <rdayismar@gmail.com>
2023-06-18 16:31:38 -0500

    feat: :construction: Initial HTML Structure + Fetch Request Test.
    The basic HTML structure of the blog has been created, and tests have been conducted using the API endpoints.

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ import myHeader from "./components/myHeader.js";
 import myFooter from "./components/myFooter.js";
 import myBanner from "./components/myBanner.js";
 import myArticles from "./components/myArticles.js";
+
+
+const fechtApi = async () => {
+    const response = await fetch("https://newsapi.org/v2/everything?q=sports&searchIn=content&language=es&pageSize=20&apiKey=f19fa1d5f0d243b2a6ff99ccf04fce40");
+    const data = await response.json();
+    console.log(data); 
+}
+
+fechtApi();
