569f1cef98d0dc20771ac7d2e458bde2b4fb412a
rrdmp15 <rdayismar@gmail.com>
2023-06-19 15:41:25 -0500

    docs: :memo: The code in the files myHeader.js and wsMyHeader.js was commented to improve the code description, and unnecessary files were removed.

--- components/myHeader.js
+++ components/myHeader.js
@@ -1,15 +1,23 @@
-export default{
-    show(){
-        let ws = new Worker('./storage/wsMyHeader.js');
-        let banner = document.querySelector('body > header > .banner > ul');
-        
-
-        ws.onmessage = (e)=>{
-            let {message, data} = e.data;
-
-            if(message === 'banner'){
-                banner.insertAdjacentHTML("beforeend", data);
-            }
+// Importing the module as a default export
+export default {
+    // Defining the 'show' method
+    show() {
+      // Creating a new Worker instance with the specified file
+      let ws = new Worker('./storage/wsMyHeader.js');
+      // Selecting the 'ul' element within the header's banner
+      let banner = document.querySelector('body > header > .banner > ul');
+  
+      // Event handler for receiving messages from the worker
+      ws.onmessage = (e) => {
+        // Destructuring the 'message' and 'data' properties from the received data
+        let { message, data } = e.data;
+  
+        // Checking if the message is 'banner'
+        if (message === 'banner') {
+          // Inserting the data as HTML at the end of the 'banner' element
+          banner.insertAdjacentHTML('beforeend', data);
         }
-    }
-}
+      };
+    },
+  };
+  
--- storage/wsMyHeader.js
+++ storage/wsMyHeader.js
@@ -1,31 +1,42 @@
-let plantilla;
+// Declaration of the 'template' variable
+let template;
 
+// Asynchronous function for fetching the banner
+const fetchBanner = async () => {
+  // Array containing the categories
+  let category = ['business', 'entertainment', 'health', 'science', 'sports', 'technology'];
+  // Array to store articles by category
+  let articlesByCategory = [];
 
-const fechtBanner = async () => {
-    let category = ['business', 'entertainment', 'health', 'science', 'sports', 'technology'];
-    let articlesByCategory = [];
+  // Looping over each category
+  for (const i of category) {
+    // Fetching the top headlines for the current category from the news API
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${i}&apiKey=f19fa1d5f0d243b2a6ff99ccf04fce40`);
+    // Parsing the response as JSON
+    const resul = await res.json();
 
-    for (const i of category) {
-        const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${i}&apiKey=f19fa1d5f0d243b2a6ff99ccf04fce40`);
-        const resul = await res.json();
+    // Extracting the first article from the response
+    let firstArticle = resul.articles[0];
 
-        let firtsArticle = resul.articles[0];
+    // Creating an HTML template with the article information
+    template = `
+      <li>
+        <img src="${firstArticle.urlToImage}" alt="">
+        <div>
+          <h2>${firstArticle.title}</h2>
+          <p>${firstArticle.description}</p>
+          <a href="${firstArticle.url}" target="_blank">Read More</a>
+        </div>
+      </li>
+    `;
 
-        let plantilla = `
-        <li>
-            <img src="${firtsArticle.urlToImage}" alt="">
-            <div>
-                <h2>${firtsArticle.title}</h2>
-                <p>${firtsArticle.description}</p>
-                <a href="${firtsArticle.url}" target="_blank">Read More</a>
-            </div>
-        </li>
-        `
+    // Adding the template to the 'articlesByCategory' array
+    articlesByCategory.push(template);
+  }
 
-        articlesByCategory.push(plantilla);
-    }
-    
-    postMessage({message: 'banner', data: articlesByCategory});
-}
+  // Sending a message to the main thread with the banner data
+  postMessage({ message: 'banner', data: articlesByCategory });
+};
 
-fechtBanner();
+// Calling the fetchBanner function
+fetchBanner();
